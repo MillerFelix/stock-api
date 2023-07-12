54dc16203143573a0cf089f8a481b951c7f1c94b
Miller <millerredigolo@gmail.com>
2023-07-12 14:20:36 -0300

    Creating and configuring project

---
+++ src/main/java/com/kaspperacademy/stockapi/StockApiApplication.java
@@ -0,0 +1,13 @@
+package com.kaspperacademy.stockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockApiApplication.class, args);
+	}
+
+}
