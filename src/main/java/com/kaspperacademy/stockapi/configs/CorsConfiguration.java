6c8dbaea327e34865904721cc36926badcb52798
Miller <millerredigolo@gmail.com>
2023-08-11 20:47:34 -0300

    Request permission file

---
+++ src/main/java/com/kaspperacademy/stockapi/configs/CorsConfiguration.java
@@ -0,0 +1,14 @@
+package com.kaspperacademy.stockapi.configs;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "DELETE", "OPTIONS", "HEAD");
+    }
+}
