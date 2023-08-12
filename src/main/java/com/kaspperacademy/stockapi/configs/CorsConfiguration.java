8bd32c821f06816e86f4c9a5205894c534ac0a6f
Miller <millerredigolo@gmail.com>
2023-08-11 21:41:05 -0300

    Correction

--- src/main/java/com/kaspperacademy/stockapi/configs/CorsConfiguration.java
+++ src/main/java/com/kaspperacademy/stockapi/configs/CorsConfiguration.java
@@ -1,8 +1,9 @@
 package com.kaspperacademy.stockapi.configs;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+@Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
